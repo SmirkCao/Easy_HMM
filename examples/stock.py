51fefd05136d3300867c740b4fdb39a4ebb1ed11
smirk <smirk.cao@gmail.com>
2018-09-17 01:17:31 +0800

    pandas 处理数据

--- examples/stock.py
+++ examples/stock.py
@@ -13,22 +13,23 @@ import pandas as pd
 import numpy as np
 import argparse
 import logging
+import warnings
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger(__name__)
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-p", "--path", required=False, help="path to input data file")
-    args = vars(ap.parse_args())
+def load_data(path):
+    quotes = pd.read_csv(path, parse_dates=['Date'])
+    quotes["Close_diff"] = quotes["Close"].diff()
+    X = np.column_stack([scale(quotes["Close_diff"][1:]), scale(quotes["Volume"][1:])])
+    return quotes, X
 
-    quotes = pd.read_csv('../data/yahoofinance-INTC-19950101-20040412.csv')
 
+def load_data_old(path):
+    quotes = pd.read_csv(path)
     dates = quotes.index.values
     close_v = quotes[["Close"]].values.flatten()
     volume = quotes[["Volume"]].values.flatten()
-    # diff：out[n] = a[n+1] - a[n] 得到价格变化
     diff = np.diff(close_v)
+
     dates = dates[1:]
     close_v = close_v[1:]
     volume = volume[1:]
@@ -36,27 +37,38 @@ if __name__ == '__main__':
     # scale归一化处理：均值为0和方差为1
     # 将价格和交易数组成输入数据
     X = np.column_stack([scale(diff), scale(volume)])
+    return quotes, X
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--path", required=False, help="path to input data file")
+    args = vars(ap.parse_args())
+
+    quotes, X = load_data('../data/yahoofinance-INTC-19950101-20040412.csv')
     model = hmm.GaussianHMM(n_components=4)
     model.fit(X)
     # 预测隐状态
     _, hidden_states = model.decode(X)
 
     # 打印参数
-    print("Transition matrix: ", model.transmat_)
-    print("Means and vars of each hidden state")
+    logger.info("Transition matrix: %s" % model.transmat_)
+    logger.info("Means and vars of each hidden state")
     for i in range(model.n_components):
-        print("{0}th hidden state".format(i))
-        print("mean = ", model.means_[i])
-        print("var = ", model.covars_[i])
-        print()
+        logger.info("{0}th hidden state".format(i))
+        logger.info("mean = %s" % model.means_[i])
+        logger.info("var = %s" % model.covars_[i])
 
-    # 画图描述
     fig, axs = plt.subplots(model.n_components, sharex=True, sharey=True)
     colours = cm.rainbow(np.linspace(0, 1, model.n_components))
     for i, (ax, colour) in enumerate(zip(axs, colours)):
         # Use fancy indexing to plot data in each state.
         mask = hidden_states == i
-        ax.plot_date(dates[mask], close_v[mask], ".-", c=colour)
+        ax.plot_date(quotes[1:]["Date"][mask], quotes[1:]["Close"][mask], ".-", c=colour)
         ax.set_title("{0}th hidden state".format(i))
 
         # Format the ticks.
